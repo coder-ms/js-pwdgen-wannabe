efdc59727467e69c11f701e932a941a89fc78948
coder-ms <marco.sbrascia@gmail.com>
2022-10-06 09:13:47 +0300

    changes

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 const nameUser = prompt('What is your name ?');
 const surnameUser = prompt('What is your surname ?');
 const colorUser = prompt('What is your favourite color ?');
-let ageUser = prompt('WHow old are you ?');
+let ageUser = prompt('How old are you ?');
 
 let somma = nameUser + surnameUser + colorUser + ageUser;
-console.log('Your password is: ' + somma);
+document.getElementById("demo").innerHTML = somma;
